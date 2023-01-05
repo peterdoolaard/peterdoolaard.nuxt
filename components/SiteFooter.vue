1d2ea532d5e28fc601d51fe397e80ed08b0dfd61
peterdoolaard <peter.doolaard@xs4all.nl>
2023-01-05 08:07:57 +0100

    Working on footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,52 +1,58 @@
 <template>
   <footer class="site-footer">
     <div class="footer-contact">
-      <p>Made with ❤️ &amp; 🥳 </p>
+      <p class="flex-container" lang="en-GB">Made with
+        <span class="screen-reader-text">love and joy</span>
+        <span aria-hidden="true">❤️ &amp; 🥳 </span>
+      </p>
       <address>
-        <IconsMail />
-        <a href="mailto:hi@peterdoolaard.nl">Peter Doolaard</a>
-        <IconsPhone />
-        <a href="tel:+31623223640">06 23223640</a>
+        <a class="flex-container" href="mailto:hi@peterdoolaard.nl">
+          <IconsMail aria-hidden="true" />
+          <span class="screen-reader-text">Mail </span>hi@peterdoolaard.nl
+        </a>
+        <a class="flex-container" href="tel:+31623223640">
+          <IconsPhone aria-hidden="true" />
+          <span class="screen-reader-text">Bel </span>06 23223640
+        </a>
       </address>
     </div>
-    <div class="footer-more">
-      <p>KVK 55196071</p>
-      <p>
-        <IconsCake />
-        Celebrating {{ year }}
-      </p>
+    <div class="footer-social">
+        <a class="flex-container" href="https://github.com/peterdoolaard?tab=repositories">
+          <IconsGithub class="github" aria-hidden="true" />
+          github.com/peterdoolaard
+        </a>
+        <a class="flex-container" href="https://www.linkedin.com/in/peterdoolaard/">
+          <IconsLinkedin aria-hidden="true" />
+          linkedin.com/in/peterdoolaard
+        </a>
+        <a class="flex-container" href="https://mastodon.social/@peterdoolaard">
+          <IconsMastodon aria-hidden="true" />
+          @peterdoolaard@mastodon.social
+        </a>
     </div>
+      <p class="flex-container">KVK 55196071</p>
   </footer>
 </template>
 
 <script setup lang="ts">
-const year: number = new Date().getFullYear()
 </script>
 
 <style lang="scss" scoped>
 
 .site-footer {
+  align-items: flex-start;
+  color: white;
+  display: flex;
+  flex-flow: wrap;
   font-size: smaller;
+  gap: 1em;
   margin: 0 -1rem -1rem;
-  padding-block: 2rem 1rem;
+  padding: 2rem 1rem 1rem;
   position: relative;
-
-  > * + * {
-    margin-block-start: 1em;
-  }
-
-  @media (min-width: $breakpoint-tablet) {
-    column-gap: 1em;
-    display: flex;
-
-    > * + * {
-      margin-block-start: 0;
-    }
-  }
 }
 
 .footer-contact,
-.footer-more {
+.footer-social {
   align-items: center;
   display: flex;
   flex-flow: wrap;
@@ -58,12 +64,23 @@ address {
   display: flex;
   flex-flow: nowrap;
   font-style: normal;
-  gap: 0.5em;
+  gap: 0.75em;
 }
 
 svg {
   block-size: 1em;
   inline-size: auto;
+
+  &.github {
+    fill: currentColor;
+  }
+}
+
+.flex-container {
+  align-items: center;
+  display: flex;
+  gap: 0.5em;
+  white-space: nowrap;
 }
 
 
